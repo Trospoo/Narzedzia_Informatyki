4d178b0d70323b41196306097b38b1aaa3cdb35e
Miłosz <milosz.urbanski02@gmail.com>
2023-06-19 20:16:59 +0200

    Revert "Ta zmiana dodaje blok try-except, który obsługuje błędy wartości. Jeśli użytkownik wprowadzi nieprawidłowe dane (np. literę zamiast liczby), program wyświetli komunikat o błędzie zamiast rzucić wyjątek i zakończyć działanie programu."
    This reverts commit 9d330594ff6da5025c240791844cfcaf3d599fa8.

--- plik_1.py
+++ plik_1.py
@@ -1,16 +1,13 @@
-try:
-    waga = float(input("Podaj swoją wagę (w kg): "))
-    wzrost = float(input("Podaj swój wzrost (w metrach): "))
+waga = float(input("Podaj swoją wagę (w kg): "))
+wzrost = float(input("Podaj swój wzrost (w metrach): "))
 
-    bmi = waga / (wzrost ** 2)
-    print("Twoje BMI wynosi:", bmi)
+bmi = waga / (wzrost ** 2)
+print("Twoje BMI wynosi:", bmi)
 
-    if bmi < 18.5:
-        print("Masz niedowagę.")
-    elif bmi >= 18.5 and bmi < 25:
-        print("Twoja waga jest prawidłowa.")
-    else:
-        print("Masz nadwagę.")
-
-except ValueError:
-    print("Wprowadzono nieprawidłowe dane. Podaj wartości liczbowe.")
+# Dodatkowa funkcjonalność: Sprawdzenie zakresu wartości BMI
+if bmi < 18.5:
+    print("Masz niedowagę.")
+elif bmi >= 18.5 and bmi < 25:
+    print("Twoja waga jest prawidłowa.")
+else:
+    print("Masz nadwagę.")
