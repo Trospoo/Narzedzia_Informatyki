430a00335eaf473a33a1d58cb716d8735d561f3c
Miłosz <milosz.urbanski02@gmail.com>
2023-06-19 20:04:50 +0200

    Dodanie dwóch plików

---
+++ plik_1.py
@@ -0,0 +1,5 @@
+waga = float(input("Podaj swoją wagę (w kg): "))
+wzrost = float(input("Podaj swój wzrost (w metrach): "))
+
+bmi = waga / (wzrost ** 2)
+print("Twoje BMI wynosi:", bmi)
---
+++ plik_2.py
@@ -0,0 +1,4 @@
+celsius = float(input("Podaj temperaturę w stopniach Celsiusza: "))
+
+fahrenheit = (celsius * 9/5) + 32
+print("Temperatura w stopniach Fahrenheit wynosi:", fahrenheit)
