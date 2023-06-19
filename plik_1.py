9d7a2fd4602c87d68992555eeb343bc255eed203
Miłosz <milosz.urbanski02@gmail.com>
2023-06-19 20:08:06 +0200

    Ta zmiana dodaje dodatkową funkcjonalność do kalkulatora BMI. Po obliczeniu BMI, sprawdza ona zakres wartości i wyświetla odpowiedni komunikat, informujący użytkownika, czy ma niedowagę, prawidłową wagę czy nadwagę.

--- plik_1.py
+++ plik_1.py
@@ -3,3 +3,11 @@ wzrost = float(input("Podaj swój wzrost (w metrach): "))
 
 bmi = waga / (wzrost ** 2)
 print("Twoje BMI wynosi:", bmi)
+
+# Dodatkowa funkcjonalność: Sprawdzenie zakresu wartości BMI
+if bmi < 18.5:
+    print("Masz niedowagę.")
+elif bmi >= 18.5 and bmi < 25:
+    print("Twoja waga jest prawidłowa.")
+else:
+    print("Masz nadwagę.")
